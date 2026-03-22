5eaf99956adcd83684bed4430d044ac70bccdafb
Ayman-DevStack <ayman.dev.offical@gmail.com>
2026-03-22 16:08:06 +0300

    10-17 Data Type and Variable

---
+++ 10-17 Data Type and Veriable/project 1/main.js
@@ -0,0 +1,27 @@
+let Numberone  = "one"
+console.log( "10");
+console.log(typeof "10")
+
+let Numbertwo = "two"
+console.log ("20");
+console.log(typeof "20")
+
+console.log("1220")
+console.log(typeof 1220)
+
+let First = "900"
+let Second = "120"
+
+console.log( (First + Second))
+
+let a = "Num"
+let b = "ber"
+
+console.log( (a + b))
+
+let Number = "20"
+let Num = "10"
+console.log(20 +"\n"+ 10)
+
+console.log(`${20} 
+${10}`)
